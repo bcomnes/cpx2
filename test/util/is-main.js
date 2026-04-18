fb74727dde6759a0ba369561fafc5e9ab1e9d10e
Roman <dev@rman.dev>
2026-04-18 16:12:53 +0100

    Update is-main.js

--- test/util/is-main.js
+++ test/util/is-main.js
@@ -1,7 +1,7 @@
 import { realpathSync } from 'node:fs'
 import { pathToFileURL } from 'node:url'
 
-export default function isMain(importMetaUrl) {
+export default function isMain (importMetaUrl) {
   const entryPath = process.argv[1]
   if (!entryPath) return false
 
